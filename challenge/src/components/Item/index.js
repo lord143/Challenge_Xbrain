193dc7d5afb31bfd3b7c90579f2cdbcf6ff97f11
Lucas Martins <lucas.martins.fsa@gmail.com>
2022-06-12 15:48:54 -0300

    Produção

--- challenge/src/components/Item/index.js
+++ challenge/src/components/Item/index.js
@@ -18,36 +18,29 @@ export default function Item({ item }) {
       <Card
         component="img"
         src={item.image}
-        sx={{height: 400,
-          width: 220,
-          maxHeight: { xs: 400, md: 500 },
-          maxWidth: { xs: 200, md: 450 }}}/>
+        sx={{height: 320,
+            width: 200,
+            flex: 1
+             }}/>
 
-      <Card sx={{ flexGrow: 1,  }} align="center">
-        <Typography gutterBottom variant="h5" component="h2">
-          <h5 className="card-title">{item.title}</h5>   
-        
+      <Card sx={{ flexGrow: 1, flexWrap: 1  }} align="center">
+        <Card className="card-body">
+          <h5 className="card-title">{item.title}</h5>
           <h6 className="card-subtitle mb-2 text-muted">
             {formatPrice(item.price)}
           </h6>
-        </Typography>
-        
-        <Typography>
           <p className="card-text">{item.description}</p>
-        </Typography>
-
-        <Typography sx={{flexGrow: 1,align:"center"}}>
-          <Link
+          <a
             href="LINK"
             className="btn btn-primary"
             onClick={(e) => {
               e.preventDefault();
               dispatch(addItem(item));
-            }}>
-            Adicionar 
-            <ShoppingCartIcon/>
-          </Link>
-        </Typography>
+            }}
+          >
+            Adicionar <i className="fa fa-cart-plus" aria-hidden="true"></i>
+          </a>
+        </Card>
       </Card>
     </Typography>
   );
