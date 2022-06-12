5f76457435c18fa56b622457238905e9db8ac46f
Lucas Martins <lucas.martins.fsa@gmail.com>
2022-06-12 16:16:39 -0300

    [FIX] - criação de formulario do cliente

---
+++ challenge/src/components/Form/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Field, reduxForm } from 'redux-form'
+
+const SimpleForm = (props) => {
+  const { handleSubmit, pristine, reset, submitting } = props
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Nome</label>
+        <div>
+          <Field name="firstName" component="input" type="text" placeholder="First Name"/>
+        </div>
+      </div>      
+      <div>
+        <label>Email</label>
+        <div>
+          <Field name="email" component="input" type="email" placeholder="Email"/>
+        </div>
+      </div>
+      <div>
+        <label>Sex</label>
+        <div>
+          <label><Field name="sex" component="input" type="radio" value="male"/> Male</label>
+          <label><Field name="sex" component="input" type="radio" value="female"/> Female</label>
+        </div>
+      </div>
+     
+      <div>
+        <button type="submit" disabled={pristine || submitting}>Submit</button>
+        <button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</button>
+      </div>
+    </form>
+  )
+}
+
+export default reduxForm({
+  form: 'simple'  // a unique identifier for this form
+})(SimpleForm)
--- challenge/src/pages/CartPage/index.js
+++ challenge/src/pages/CartPage/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Cart from "../../components/Cart";
 import Layout from "../../components/Layout";
+import SimpleForm from "../../components/Form"
 
 export default function CartPage() {
   return (
     <Layout>
       <Cart />
+      <SimpleForm/>
     </Layout>
   );
 }
